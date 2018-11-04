5cd50f879bd21228f57dc3dae1a33298cbbb68d6
Michael Koleskin <world.ru.monty@gmail.com>
2018-11-04 21:06:00 +0300

    add ClientChat/wpf

---
+++ ClientChat/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClientChat.ServiceChatWPF;
+
+namespace ClientChat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, ServiceChatWPF.IServiceChatCallback
+    {
+        bool isConnected = false;
+        ServiceChatClient client;
+        int ID;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+                ID = client.Connect(tbUserName.Text);
+                tbUserName.IsEnabled = false;
+                bnConnect.Content = "Отключиться";
+                isConnected = true;
+            }
+        }
+
+        void DisconnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(ID);
+                client = null;
+                tbUserName.IsEnabled = true;
+                bnConnect.Content = "Подключиться";
+                isConnected = false;
+            }
+        }
+
+        private void bnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (isConnected)
+                DisconnectUser();
+            else
+                ConnectUser();
+
+        }
+
+        public void CallBackMsg(string msg)
+        {
+            lbChat.Items.Add(msg);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        private void tbMsg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                if (client != null)
+                {
+                    client.SendMsg(tbMsg.Text, ID);
+                    tbMsg.Text = string.Empty;
+                }
+        }
+    }
+}
