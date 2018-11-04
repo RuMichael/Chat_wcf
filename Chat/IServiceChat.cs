c1676f693b1a9e8bc2506ba73ab485ffefa8780e
Michael Koleskin <world.ru.monty@gmail.com>
2018-11-04 17:01:26 +0300

    layout wcf

---
+++ Chat/IServiceChat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Chat
+{
+
+    [ServiceContract(CallbackContract = typeof(IServerChatCallBack))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+
+        int Connect(string name);
+
+        [OperationContract]
+
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+
+        void SendMsg(string msg);
+    }
+
+    public interface IServerChatCallBack
+    {
+        void CallBackMsg(string msg);
+    }
+
+}
---
+++ Chat/ServiceChat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Chat
+{
+    
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+
+    public class ServiceChat : IServiceChat
+    {
+
+        List<ServerUser> users = new List<ServerUser>();
+        int nextID = 1;
+
+
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser { ID = nextID, Name = name, operationContext = OperationContext.Current };
+            nextID++;
+            SendMsg(user.Name + " подключился к чату!");
+            users.Add(user);
+            return user.ID;
+
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(item=>item.ID==id);
+            if (user != null) 
+            {
+                users.Remove(user);
+                SendMsg(user.Name + " покинул чат!");
+            }
+        }
+
+        public void SendMsg(string msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
